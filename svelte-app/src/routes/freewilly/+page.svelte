f90b10e4b2e432f627fae4efe893ad319e372ab7
unknown <c00300855@setu.ie>
2025-04-07 13:11:35 +0100

    finished freewilly article

--- svelte-app/src/routes/freewilly/+page.svelte
+++ svelte-app/src/routes/freewilly/+page.svelte
@@ -9,6 +9,18 @@ import {Footer} from '$lib';
 <p> The orca featured in the 1993 movie Free Willy was a real whale named Keiko, who became an icon for animal rights and the movement to end the captivity of marine mammals. Captured off the coast of Iceland when he was just two years old, Keiko spent over 20 years in various aquariums, where he was kept in small tanks and deprived of the space to swim freely. Despite his circumstances, Keiko became a star after appearing in the popular film.
 </p>
 
+<img id ="boyorca" src = "/boyorca.jpg" alt = "boy with orca whale">
+
+<h2> A Public Outcry and Efforts for Release</h2>
+<p> Following the movie’s success, a public outcry for Keiko’s release began. In 1996, marine mammal experts launched an ambitious rehabilitation project to prepare Keiko for life in the wild. After being moved to a larger facility in Oregon, Keiko was eventually released into the waters off Iceland in 2002. Though he swam freely, Keiko struggled to integrate with wild orca pods and, sadly, passed away in 2003 due to pneumonia.
+</p>
+
+<img id ="bigorca" src = "/bigorca.jpg" alt = "big orca whale behind glass window with small boy beneath">
+
+<h2> Keiko’s Legacy and Impact </h2>
+<p> Keiko’s story sparked a global conversation about the ethics of keeping orcas and other marine mammals in captivity for entertainment. His journey led to increased pressure on aquariums and marine parks, including SeaWorld, to end orca breeding programs and live shows. Keiko’s legacy lives on as a symbol of the need for better treatment and conservation of marine life, inspiring efforts to protect wild orcas and other endangered species.
+</p>
+
 <style>
     h1 {
         font-family: 'Courier New', Courier, monospace;
@@ -27,4 +39,7 @@ import {Footer} from '$lib';
         text-align: left;
     }
 
+    #boyorca {
+    transform: scale(0.8);
+    }
 </style>
